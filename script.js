72372385f758c425e3bc6246e46ec79fb16b779a
Azt3chn0 <manageratztechno@gmail.com>
2026-02-10 07:42:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function updateButtonClicked(){
     window.location.href = "http://127.0.0.1:5500/Website1/index.html"
 }
 function welcomeButtonClicked(){
-    window.location.href = ""
+    window.location.href = "https://azt3chn0.github.io/WelcomePage/"
 }
